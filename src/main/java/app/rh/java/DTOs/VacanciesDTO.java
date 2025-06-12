d2f1cbd90ded7a52349917d2983f0669b2838db0
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-06-12 14:13:50 -0300

    feat: created and tested the rote http://localhost:8080/Vacancies/ onmethod GET

--- src/main/java/app/rh/java/DTOs/VacanciesDTO.java
+++ src/main/java/app/rh/java/DTOs/VacanciesDTO.java
@@ -1,4 +1,52 @@
 package app.rh.java.DTOs;
 
+import jakarta.validation.constraints.NotEmpty;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class VacanciesDTO {
+
+    @NotEmpty
+    private String Name;
+
+    @NotEmpty
+    private String description;
+
+    @NotEmpty
+    private LocalDate date;
+
+    private BigDecimal wage;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public BigDecimal getWage() {
+        return wage;
+    }
+
+    public void setWage(BigDecimal wage) {
+        this.wage = wage;
+    }
 }
--- src/main/java/app/rh/java/controllers/VacanciesController.java
+++ src/main/java/app/rh/java/controllers/VacanciesController.java
@@ -1,12 +1,33 @@
 package app.rh.java.controllers;
 
+import app.rh.java.entitys.Vacancies;
 import app.rh.java.repositiry.VacanciesRepository;
+import app.rh.java.repositiry.CandidateRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/Vacancies")
 public class VacanciesController {
    private VacanciesRepository vacanciesRepository;
-    private candidateRepository vacanciesRepository;
+   private CandidateRepository candidateRepository;
+
+    public VacanciesController(VacanciesRepository vacanciesRepository, CandidateRepository candidateRepository) {
+        this.vacanciesRepository = vacanciesRepository;
+        this.candidateRepository = candidateRepository;
+    }
+
+    @GetMapping("/")
+   public List<Vacancies> GetVacancies(){
+     return vacanciesRepository.findAll();
+   }
+
+   @
+
 
 
 }
