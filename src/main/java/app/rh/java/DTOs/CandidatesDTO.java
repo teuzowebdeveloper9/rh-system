c5d89f6e3c3beab548509a872094ab28c8f38897
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-06-12 17:09:44 -0300

    feat : created the rote POST for create candidates and testes this rote http://localhost:8080/candidates/ with this body {   "cpf" : "732937462-4",   "email": "mateustesteEmail@gmail.com",   "name": "mateus silva oliveira" } return {     "cpf": "732937462-4",     "name": "mateus silva oliveira",     "email": "mateustesteEmail@gmail.com",     "vacancies": null,     "id": "8dd58231-f4ac-4182-a9f2-99e19e3b62e9" } PERFECT

--- src/main/java/app/rh/java/DTOs/CandidatesDTO.java
+++ src/main/java/app/rh/java/DTOs/CandidatesDTO.java
@@ -12,4 +12,28 @@ public class CandidatesDTO {
 
      @NotEmpty
     private String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/app/rh/java/controllers/CandidatesController.java
+++ src/main/java/app/rh/java/controllers/CandidatesController.java
@@ -1,10 +1,12 @@
 package app.rh.java.controllers;
 
+import app.rh.java.DTOs.CandidatesDTO;
 import app.rh.java.entitys.Candidates;
 import app.rh.java.repositiry.CandidateRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +23,19 @@ public class CandidatesController {
     @GetMapping("/")
     public List<Candidates> getAllCandidates(){
         return this.candidateRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Candidates> createCandidates(@Valid @RequestBody CandidatesDTO candidatesDTO){
+        Candidates candidate = new Candidates();
+
+        candidate.setCpf(candidatesDTO.getCpf());
+        candidate.setEmail(candidatesDTO.getEmail());
+        candidate.setName(candidatesDTO.getName());
+
+        Candidates savedCandidate = candidateRepository.save(candidate);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCandidate);
+
     }
 }
